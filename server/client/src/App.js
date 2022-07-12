0c27d86f6dee2a0cedb51e6ecd8ecd7810dc59e7
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-07-13 02:02:25 +0300

    Moving to the Client Side and setup proxy with http-proxy-middleware

---
+++ server/client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hi, There</p>
+        <a className="App-link" href="/auth/google">
+          Sign in with google Auth
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
