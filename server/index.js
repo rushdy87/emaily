6d7a87afe039ab9c7f7b7e6df763b19ff7493089
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-07-17 12:13:36 +0300

    Back End to Front End Routing in Production

--- server/index.js
+++ server/index.js
@@ -29,6 +29,22 @@ require("./routes/authRoutes")(app);
 /* we require the function from authRoutes file, and invoked it in the same time */
 require("./routes/billingRoutes")(app);
 
+// start: the code at the bottom work in the production environment
+if (process.env.NODE_ENV === "production") {
+  // 1- Express will serve up production assets like main.js, or main.css
+  app.use(express.static("client/build")); //built-in middleware for serves static.
+
+  // 2- Express will serve up production index.html, if it doesn't recognize the route
+  const path = require("path"); //The Path module provides a way of working with directories and file paths.
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    // 3- The path.resolve() resolves a sequence of paths or path segments into an absolute path.
+    // 4- The __dirname returns the path of the folder where the current JavaScript file resides.
+    // 5- The Order important, app.get('*') must be the last route in the app
+  });
+}
+// end: the code above end here
+
 const PORT = process.env.PORT || 5000; /* In many environments (e.g. Heroku),
 and as a convention, you can set the environment variable PORT to tell your
 web server what port to listen on.
